a3f1ac2e612baba48c49ef2300d25d45b72a7697
dmt4-0-4 <dmitriy_bordunov@student.itpu.uz>
2025-06-09 13:48:45 +0500

    Done

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require('cssnano')({ preset: 'default' }),
+    purgecss({
+      content: ['./src/**/*.html', './src/**/*.js', './src/**/*.scss'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+  ]
+}
